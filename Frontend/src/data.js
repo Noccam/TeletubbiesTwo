796582d849f00e7f69cc73d0ffe9b8bbd9f7a34f
Noccam <cameron.c.plank@gmail.com>
2021-08-13 19:19:09 -0700

    Initial commit

---
+++ Frontend/src/data.js
@@ -0,0 +1,76 @@
+export default {
+    products : [
+        {
+            _id: '1',
+            name: 'Full Zip Running Shirt',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '2',
+            name: 'Long Sleeve Shirt',
+            category: 'Shirts',
+            image: '/images/product-2.jpg',
+            price: 60,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '3',
+            name: 'Full Zip Hoodie',
+            category: 'Shirts',
+            image: '/images/product-3.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '4',
+            name: 'Big Chungus Shirt',
+            category: 'Shirts',
+            image: '/images/product-4.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        
+        {
+            _id: '5',
+            name: 'Joe Mama Shirt',
+            category: 'Shirts',
+            image: '/images/product-5.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+
+        {
+            _id: '6',
+            name: 'Smosh Shirt',
+            category: 'Shirts',
+            image: '/images/product-6.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        
+    ],
+};
